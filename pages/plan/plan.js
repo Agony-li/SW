f9affe8e3157e2336e9c015a489bd9f7362c4651
liwei <15010199100@163.com>
2020-11-23 22:18:33 +0800

    计划部分

--- pages/plan/plan.js
+++ pages/plan/plan.js
@@ -7,7 +7,28 @@ Page({
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     dialogphone: true,
+    plan_ready: false, // 是否做好计划前准备
     userInfo: '',
+    options: [
+      {
+        status: false
+      },
+      {
+        status: false
+      },
+      {
+        status: false
+      },
+      {
+        status: false
+      },
+      {
+        status: false
+      },
+      {
+        status: false
+      },
+    ]
    },
    onReady: function (e) {
      this.setData({
@@ -27,6 +48,42 @@ Page({
        url: '../plan/planExplain',
      })
    },
+
+   // 选中options
+   chooseOption(e){
+     let index = e.currentTarget.dataset.index
+     let status = "options["+index+"].status"
+     if(this.data.options[index].status){
+      this.setData({
+        [status]: false
+      })
+     }else {
+      this.setData({
+        [status]: true
+      })
+     }
+    
+   },
+   
+  // 完成准备
+  finishPrepare(){
+    console.log(this.data.options);
+    if(this.data.options.findIndex(obj => obj.status === false) == -1){
+      console.log('验证通过')
+      this.setData({
+        plan_ready: true
+      })
+    }else{
+      console.log('验证不通过')
+    }
+  },
+
+  // 跳转到课程页
+  gotoCourseList(){
+    wx.navigateTo({
+      url: '../course/courselist',
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
