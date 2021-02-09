2a9ccea1da288cac7ef66b9cbd7788d65aff8a77
liwei <15010199100@163.com>
2021-02-09 22:07:28 +0800

    优惠券

--- components/myCoupons/index.js
+++ components/myCoupons/index.js
@@ -1,18 +1,24 @@
-// components/myCoupons/index.js
+//index.js
+import util from '../../utils/util.js';
+import config from '../../utils/dev.config.js';
+import regeneratorRuntime from '../../common/regenerator-runtime/runtime';
+var  WxParse= require('../../wxParse/wxParse.js');
+//获取应用实例
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    couponType: "0007"
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      this.loadCoupons()
   },
 
   /**
@@ -62,5 +68,28 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  async loadCoupons() {
+    let data = await util.httpRequestWithPromise('/rest/coupon/list?type=my', 'GET', '', wx.getStorageSync('key'));
+    if(data.statusCode === 200){
+		console.log(data.data.data)
+		this.setData({
+			list:data.data.data,
+		});
+    }
+  },
+  async confirm(o) {
+    const pages = getCurrentPages()
+    const prevPage = pages[pages.length-2] // 上一页
+    // 调用上一个页面的setData 方法，将数据存储
+    prevPage.setData({
+      couponId: o.currentTarget.dataset.id
+    })
+    prevPage.loadCoupon();
+    // 返回上一页
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
