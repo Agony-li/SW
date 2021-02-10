4d74e32c64f336ec999ff5758afb9730c189eaa1
liwei <15010199100@163.com>
2021-02-10 22:14:57 +0800

    .

--- components/myRefund/myRefundRules.js
+++ components/myRefund/myRefundRules.js
@@ -1,19 +1,36 @@
 // components/myRefund/myRefundRules.js
+import util from '../../utils/util.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    content: ``
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取奖金规则
+    let id = '1359492299438600192'
+    this.getMyBonusRules(id)
   },
+
+  // 获取奖金规则
+  async getMyBonusRules(id){
+    let data = await util.httpRequestWithPromise(`/rest/cms/article?id=${id}`, 'get', '', wx.getStorageSync('key'));
+    console.log('获取奖金规则', data.data.data)
+    if (data.statusCode === 200) {
+      if(data.data.data.articleData.content!=undefined){
+        this.setData({
+          content:data.data.data.articleData.content,
+        })
+      }
+    }
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
